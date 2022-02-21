27638497fa164bb34930e04caddff925c072f29f
Akashhhhhh <96081139+Akashhhhhh@users.noreply.github.com>
2022-02-21 16:54:57 +0530

    evaluation

--- unit-4/sprint-4/evaluation/src/index.js
+++ unit-4/sprint-4/evaluation/src/index.js
@@ -3,12 +3,16 @@ const express = require("express")
 const connect = require("./configs/db")
 const usercontroller = require("./controllers/user.controller")
 const postcontroller = require("./controllers/post.controller")
+const postlike = require("./controllers/postlike.controller")
+const comment = require("./controllers/comment.controller")
 const app = express()
 
 app.use(express.json())
 
 app.use("/users", usercontroller)
 app.use("/post", postcontroller)
+app.use("/postlike", postlike)
+app.use("/comment", comment)
 
 
 app.listen(4500, async function(){
--- unit-4/sprint-4/evaluation/src/models/user.model.js
+++ unit-4/sprint-4/evaluation/src/models/user.model.js
@@ -6,12 +6,23 @@ const userSchema = new mongoose.Schema(
         lastName :  {type: String,  required:false},
         age:        {type: Number,  required:true},
         email :     {type: String,  required: true, unique: true},
-        profileImages : {type:String, required: false}
+        profileImages : {type:String, required: false},
+        password : {type:String, required: true}
     },
     {
         versionKey:false,
         timestamps:true
     }
 )
+userSchema.pre("save", function(next){
+    if(!this.isModified("password")) return next()
 
+    var hash = bcrypt.hashSync(this.password, 8);
+    this.password = hash;
+    return next()
+})
+
+userSchema.methods.checkPass = function (password){
+    return bcrypt.compareSync(password, this.password); 
+}
 module.exports = mongoose.model("user", userSchema)
